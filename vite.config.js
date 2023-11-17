83cde66f7e390a006a6fa845426b633f52387bff
Chamoli <godchamoli@gmail.com>
2023-11-17 13:32:58 +0530

    Deploying Frontend trial 1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    "/api": "http://localhost:5000",
+    proxy: {
+      '/api': {
+        target: 'https://moderndiseasedetectorbackend.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 
   plugins: [react()],
